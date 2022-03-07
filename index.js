7ea2cc0395c6b8f388725cfd4b7eb735362082a5
UiguunaMikhailova <uyguuna1997@yandex.ru>
2022-03-07 03:21:00 +0300

    feat: add functions for projects slider

--- index.js
+++ index.js
@@ -1,5 +1,34 @@
-const projectCard = document.querySelectorAll('.project1')
+const slides = document.querySelectorAll('.project1')
 const back = document.querySelector('.back')
 const next = document.querySelector('.next')
 
+let index = 0
+console.log(slides)
 
+back.addEventListener('click',  prevSlide);
+next.addEventListener('click',  nextSlide);
+
+function activeSlide(i) {
+    for (let slide of slides) {
+        slide.classList.remove('active');
+    }
+    slides[i].classList.add('active');
+};
+function nextSlide() {
+    if (index === slides.length - 1) {
+        index = 0;
+        activeSlide(index);
+    } else {
+        index++;
+        activeSlide(index);
+    }
+};
+function prevSlide() {
+    if (index === 0) {
+        index = slides.length - 1;
+        activeSlide(index);
+    } else {
+        index--;
+        activeSlide(index);
+    }
+};
